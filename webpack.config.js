e4c1d6f197160ad7edbecd0aa1097141dda6aabd
Elliot Hesp <elliot.hesp@gmail.com>
2016-02-02 17:02:45 +0000

    Add development env to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,11 @@ module.exports = {
         }]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"development"'
+            }
+        })
     ]
 };
